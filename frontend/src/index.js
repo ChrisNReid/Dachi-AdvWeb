7c56f9411ba24706a619339d2a19256dedec52d5
ChrisNReid <66421358+ChrisNReid@users.noreply.github.com>
2023-05-04 17:35:17 +0100

    remove empty files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,17 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-// const express = require('express');
-// const app = express();
-
-
-// Body parser middleware
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-
-// Import authentication routes
-// const authRoutes = require('./auth');
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -21,6 +10,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
